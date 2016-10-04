97a4128db8412836f1635fc2bcb1998699ec67af
Thomas Deutschmann <whissi@whissi.de>
2016-10-04 19:13:26 +0200

    Fix code indentation
    Use TAB instead of SPACE character.

--- turbo_realpath.c
+++ turbo_realpath.c
@@ -11,24 +11,24 @@
 #include "turbo_realpath.h"
 
 static zend_function_entry turbo_realpath_functions[] = {
-    {NULL, NULL, NULL}
+	{NULL, NULL, NULL}
 };
 
 zend_module_entry turbo_realpath_module_entry = {
 #if ZEND_MODULE_API_NO >= 20010901
-    STANDARD_MODULE_HEADER,
+	STANDARD_MODULE_HEADER,
 #endif
-    PHP_TURBO_REALPATH_EXTNAME,
-    turbo_realpath_functions,
-    PHP_MINIT(turbo_realpath),
-    PHP_MSHUTDOWN(turbo_realpath),
-    PHP_RINIT(turbo_realpath),
-    NULL,
-    PHP_MINFO(turbo_realpath),
+	PHP_TURBO_REALPATH_EXTNAME,
+	turbo_realpath_functions,
+	PHP_MINIT(turbo_realpath),
+	PHP_MSHUTDOWN(turbo_realpath),
+	PHP_RINIT(turbo_realpath),
+	NULL,
+	PHP_MINFO(turbo_realpath),
 #if ZEND_MODULE_API_NO >= 20010901
-    PHP_TURBO_REALPATH_VERSION,
+	PHP_TURBO_REALPATH_VERSION,
 #endif
-    STANDARD_MODULE_PROPERTIES
+	STANDARD_MODULE_PROPERTIES
 };
 
 ZEND_GET_MODULE(turbo_realpath)
@@ -41,60 +41,60 @@ PHP_INI_END()
 
 PHP_RINIT_FUNCTION(turbo_realpath)
 {
-    char *basedir = INI_STR("realpath_cache_basedir");
-    char *safe_mode = INI_STR("realpath_cache_safe_mode");
-    char *disabled_functions = INI_STR("disable_functions");
-    char *risky_functions = "link,symlink";
-    char *new_functions;
-    int security = INI_INT("realpath_cache_security");
+	char *basedir = INI_STR("realpath_cache_basedir");
+	char *safe_mode = INI_STR("realpath_cache_safe_mode");
+	char *disabled_functions = INI_STR("disable_functions");
+	char *risky_functions = "link,symlink";
+	char *new_functions;
+	int security = INI_INT("realpath_cache_security");
 
-    if(strlen(basedir) > 0) {
-        zend_alter_ini_entry("open_basedir", sizeof("open_basedir"), basedir, strlen(basedir), PHP_INI_SYSTEM, PHP_INI_STAGE_ACTIVATE);
-    }
+	if(strlen(basedir) > 0) {
+		zend_alter_ini_entry("open_basedir", sizeof("open_basedir"), basedir, strlen(basedir), PHP_INI_SYSTEM, PHP_INI_STAGE_ACTIVATE);
+	}
 
-    if(strlen(safe_mode) > 0) {
-        zend_alter_ini_entry("safe_mode", sizeof("safe_mode"), safe_mode, strlen(safe_mode), PHP_INI_SYSTEM, PHP_INI_STAGE_ACTIVATE);
-    }
+	if(strlen(safe_mode) > 0) {
+		zend_alter_ini_entry("safe_mode", sizeof("safe_mode"), safe_mode, strlen(safe_mode), PHP_INI_SYSTEM, PHP_INI_STAGE_ACTIVATE);
+	}
 
-    switch(security) {
+	switch(security) {
 	// check disabled functions for symlink and link entries
 	case 1:
-	    if(strlen(disabled_functions) > 0) {
+		if(strlen(disabled_functions) > 0) {
 		new_functions = emalloc(strlen(disabled_functions) + strlen(risky_functions) + 2);
 		strcpy(new_functions, risky_functions);
 		strcat(new_functions, ",");
 		strcat(new_functions, disabled_functions);
-	    } else {
+		} else {
 		new_functions = emalloc(strlen(risky_functions) + 1);
 		strcpy(new_functions, risky_functions);
-	    }
-	    zend_alter_ini_entry("disable_functions", sizeof("disable_functions"), new_functions, strlen(new_functions), PHP_INI_SYSTEM, PHP_INI_STAGE_ACTIVATE);
-	    efree(new_functions);
-	    break;
+		}
+		zend_alter_ini_entry("disable_functions", sizeof("disable_functions"), new_functions, strlen(new_functions), PHP_INI_SYSTEM, PHP_INI_STAGE_ACTIVATE);
+		efree(new_functions);
+		break;
 	default:
-	    break;
-    }
+		break;
+	}
 }
 
 PHP_MINIT_FUNCTION(turbo_realpath)
 {
-    REGISTER_INI_ENTRIES();
-    return SUCCESS;
+	REGISTER_INI_ENTRIES();
+	return SUCCESS;
 }
 
 PHP_MSHUTDOWN_FUNCTION(turbo_realpath)
 {
-    UNREGISTER_INI_ENTRIES();
-    return SUCCESS;
+	UNREGISTER_INI_ENTRIES();
+	return SUCCESS;
 }
 
 PHP_MINFO_FUNCTION(turbo_realpath)
 {
-    php_info_print_table_start();
-    php_info_print_table_header(2, "turbo_realpath support", "enabled");
-    php_info_print_table_row(2, "turbo_realpath version", PHP_TURBO_REALPATH_VERSION);
-    php_info_print_table_row(2, "turbo_realpath creator", "Artur Graniszewski");
-    php_info_print_table_end();
+	php_info_print_table_start();
+	php_info_print_table_header(2, "turbo_realpath support", "enabled");
+	php_info_print_table_row(2, "turbo_realpath version", PHP_TURBO_REALPATH_VERSION);
+	php_info_print_table_row(2, "turbo_realpath creator", "Artur Graniszewski");
+	php_info_print_table_end();
 
-    DISPLAY_INI_ENTRIES();
+	DISPLAY_INI_ENTRIES();
 }
